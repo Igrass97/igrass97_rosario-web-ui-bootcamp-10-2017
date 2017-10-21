e08b3a55c6d5a385dd149732547118ce2d01014d
igrass97 <igrass97@gmail.com>
2017-10-21 02:41:04 -0300

    Added the ex1 (two differents ways)

---
+++ Topic 1 - JS/Ex1 (without inline css)/main.js
@@ -0,0 +1,5 @@
+/*Removing class to make it*/
+
+function fadeIn(){
+  document.getElementById("fading-element").classList.remove("hidden");
+}
---
+++ Topic 1 - JS/Ex1/main.js
@@ -0,0 +1,7 @@
+/*Modifying inline css*/
+
+function fadeIn(){
+  document.getElementById("hidden").style.opacity = "1";
+  document.getElementById("hidden").style["font-size"] = "36px";
+  document.getElementById("hidden").style.transition = "all 10s linear";
+}
