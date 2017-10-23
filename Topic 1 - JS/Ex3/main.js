1b49f0bc85eecb177826db4d6fd86e279d10c22b
igrass97 <igrass97@gmail.com>
2017-10-23 19:20:53 -0300

    Fixed indentations

--- Topic 1 - JS/Ex3/main.js
+++ Topic 1 - JS/Ex3/main.js
@@ -21,20 +21,20 @@ const chuckApi = {
 }
 
 function makePromise(config){
-	return new Promise(function(resolve,reject){
-		let xhr = new XMLHttpRequest();
-		xhr.responseType = "json";
-		xhr.open(config.method, config.url, config.async);
-		xhr.onload = function(){
-			if (xhr.status === 200){
-				resolve(xhr.response);
+  return new Promise(function(resolve,reject){
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.open(config.method, config.url, config.async);
+    xhr.onload = function(){
+      if (xhr.status === 200){
+        resolve(xhr.response);
       } else {
         reject(xhr.statusText);
       }
-		};
-		xhr.onerror = function(){
-			reject(xhr.statusText);
-		};
-		xhr.send();
-	});
+    };
+    xhr.onerror = function(){
+      reject(xhr.statusText);
+    };
+    xhr.send();
+  });
 }
