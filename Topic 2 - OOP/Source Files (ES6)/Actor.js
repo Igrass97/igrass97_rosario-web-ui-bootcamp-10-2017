4a65860fd241d63fa251f968a9980e2827d6a9df
igrass97 <igrass97@gmail.com>
2017-10-26 18:29:48 -0300

    Deleting all files to work with Gulp and Babel

--- Topic 2 - OOP/Source Files (ES6)/Actor.js
+++
@@ -1,8 +0,0 @@
-class Actor {
-  constructor(name, age){
-    this._name = name;
-    this._age = age;
-  }
-}
-
-module.exports = Actor;
--- Topic 2 - OOP/Source Files (ES6)/script.js
+++
@@ -1,9 +0,0 @@
-import EventEmitter from "./EventEmitter.js";
-import Actor from "./Actor.js";
-import Movie from "./Movie.js";
-import Logger from "./Logger.js";
-
-let sw = new Movie("sw", 13, 13);
-let logger = new Logger();
-sw.on("play", logger.log);
-sw.play();
