739a74033f08a0348ad1c90f7893d2b55a187a3a
igrass97 <igrass97@gmail.com>
2017-10-31 12:40:11 -0300

    Created routes

---
+++ Topic 3 - Angular/My-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { routes } from "./app.router";
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    routes
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
